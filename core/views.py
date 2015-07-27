1663d3174317260156f80d0900af714d38d167ec
projectramo <projectramo@gmail.com>
2015-07-27 16:46:37 -0400

    Added images capability

--- core/views.py
+++ core/views.py
@@ -2,7 +2,20 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.views.generic import TemplateView
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+import core.models as coremodels
 
 class LandingView(TemplateView):
     template_name = "base/index.html"
+
+class LocationListView(ListView):
+    model = coremodels.Location
+    template_name = "location/list.html"
+#    template_name = 'base/theme.html' if you want to take a look
+
+class LocationDetailView(DetailView):
+    model = coremodels.Location
+    template_name = "location/detail.html"
+    context_object_name = 'location'
 
