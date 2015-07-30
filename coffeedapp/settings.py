ba6e1007a2a5940e1b2e6fc2c4f75d9aa8c6d2e9
projectramo <projectramo@gmail.com>
2015-07-30 15:25:21 -0400

    Final version of course.

--- coffeedapp/settings.py
+++ coffeedapp/settings.py
@@ -31,6 +31,16 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+TEMPLATE_CONTEXT_PROCESSORS = ("django.contrib.auth.context_processors.auth",
+"django.core.context_processors.debug",
+"django.core.context_processors.i18n",
+"django.core.context_processors.media",
+"django.core.context_processors.static",
+"django.core.context_processors.tz",
+"django.contrib.messages.context_processors.messages",
+"django.core.context_processors.request"
+)
+
 INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
@@ -39,6 +49,11 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'widget_tweaks',
+    'sitegate',
+    'bootstrap3',
+    'geoposition',
+    'bootstrap_pagination',
 )
 
 MIDDLEWARE_CLASSES = (
